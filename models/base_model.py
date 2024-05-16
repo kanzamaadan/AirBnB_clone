57cf585525f91b7b5c7e8e995c5b5d1afa894b17
kanza maadan <kenza maadan@gmail.com>
2024-05-16 10:31:44 -0700

    0

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
-        obj_dict = self.__class__.__name__
+        obj_dict['__class__'] = self.__class__.__name__
         return obj_dict
 
     def save(self):
