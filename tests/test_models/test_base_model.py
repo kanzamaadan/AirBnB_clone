8e500869bacaf2d9a7fb2d69d366f95c591477d7
daniels8945 <obaj.daniels@gmail.com>
2024-05-19 15:54:24 -0700

    tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBaseModel_save
+    TestBaseModel_to_dict
+"""
+import unittest
+import unittest.test
+
+class TestBaseModel_Instantation(unittest.TestCase):
+    """Unittests for testing instantiation of the BaseModel class."""
+    pass
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines unittests for models/engine/file_storage.py.
+
+Unittest classes:
+    TestFileStorage_instantiation
+    TestFileStorage_methods
+"""
+
+import unittest
+import unittest.testcase
+
+class TestFileStorage_instantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the FileStorage class."""
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
