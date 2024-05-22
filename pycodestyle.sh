81a7119c83f6e9d5604a4fe4b41c979aa0675cf5
daniels8945 <obaj.daniels@gmail.com>
2024-05-21 18:54:42 -0700

    pycodestyle.sh

--- pycodestyle.sh
+++ pycodestyle.sh
@@ -3,6 +3,7 @@
 pycodestyle *.py ; cd models ; pycodestyle *.py ; cd engine ;
 pycodestyle *.py ; cd ../../tests ; pycodestyle *.py ;
 cd test_models ; pycodestyle *.py ; cd  test_engine ;
+pycodestyle *.py ; cd ../../../
 
 echo "Pycode style was ran in:"
 echo "./"
